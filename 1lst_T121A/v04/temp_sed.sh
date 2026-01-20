c8fe17cbf7288d03c2e7dfd9549def64dfbe618a
Darian T. Yang <dty7@pitt.edu>
2026-01-20 04:14:30 -0500

    ran setup script and set up dirs

---
+++ 1lst_T121A/v04/temp_sed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# temp_sed.sh
+# replace 1lst_T121A and v04 in the std sim scripts dir with target PDB
+
+# Parameters
+# ----------
+# arg 1 = pdb system prefix         :   e.g. 2kod_wt
+# arg 2 = version of the replicate  :   e.g. v00
+
+PDB=$1
+v04=$2
+
+# apply globally to all files in current directory
+sed -i "s/1lst_T121A/${PDB}/g" *
+sed -i "s/v04/${VER}/" * 
---
+++ 1lst_T121K/v01/temp_sed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# temp_sed.sh
+# replace 1lst_T121K and v01 in the std sim scripts dir with target PDB
+
+# Parameters
+# ----------
+# arg 1 = pdb system prefix         :   e.g. 2kod_wt
+# arg 2 = version of the replicate  :   e.g. v00
+
+PDB=$1
+v01=$2
+
+# apply globally to all files in current directory
+sed -i "s/1lst_T121K/${PDB}/g" *
+sed -i "s/v01/${VER}/" * 
