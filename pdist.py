45a335a2be1d0e5ac9e06f24012a5829dbfe2a2d
Darian T. Yang <dty7@pitt.edu>
2026-01-22 06:05:12 -0500

    added wt noliz data and plotted

--- pdist.py
+++ pdist.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 plt.style.use("~/Apps/wedap/styles/default.mplstyle")
 
-variant = "1lst_T121K"
+#variant = "1lst_T121K"
+variant = "noliz"
 
 def wepr_plot():
     plot_options = {"xlim" : (30,90),
@@ -16,9 +17,11 @@ def wepr_plot():
                     "p_units" : "kcal",
                     "title" : variant, 
                     }
-    pc = [f"{variant}/v{i:02d}/06_pcoord.dat" for i in range(1,6)]
+    #pc = [f"{variant}/v{i:02d}/06_pcoord.dat" for i in range(1,6)]
+    pc = [f"{variant}/rep{i}.dat" for i in range(1,6)]
     mdap.MD_Plot(Xname=pc, Xindex=1, Yname=pc, Yindex=0, data_type="pdist", **plot_options).plot()
 
 wepr_plot()
-plt.savefig(f"06_pdist_{variant}.pdf")
+#plt.savefig(f"06_pdist_{variant}.pdf")
+plt.savefig(f"pdist_{variant}.pdf")
 #plt.show()
