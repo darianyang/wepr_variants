75e4ba60452a1c9031cb211cc9c5545d0bb5c210
Darian T. Yang <dty7@pitt.edu>
2026-02-25 05:00:21 -0500

    prep for full 1µs run extensions

--- run_multi2.sh
+++ run_multi2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-SYSTEMS=(1lst_T121A 1lst_T121K)
-#SYSTEMS=(1lst_Y14A 1lst_T121A-Y14A)
+#SYSTEMS=(1lst_T121A 1lst_T121K)
+SYSTEMS=(1lst_T121A 1lst_WT)
+#SYSTEMS=(1lst_T121K 1lst_Y14A 1lst_T121A-Y14A)
 
 for SYSTEM in ${SYSTEMS[@]} ; do
 cd $SYSTEM
