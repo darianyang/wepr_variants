5ec8d519bd0f733af70e1a54be2de54712141803
Darian T. Yang <dty7@pitt.edu>
2026-03-06 07:30:14 -0500

    updated ylim on full 600ns plots

--- pdist.py
+++ pdist.py
@@ -25,15 +25,15 @@ def wepr_plot(variant, ax=None, title=None):
                     }
     #pc = [f"{variant}/v{i:02d}/06_pcoord.dat" for i in range(1,6)]
     #pc = [f"{variant}/v{i:02d}/0{j}_pcoord.dat" for i in range(1,6) for j in range(6,9)]
-    pc = [f"{variant}/v{i:02d}/0{j}_pcoord.dat" for i in range(1,6) for j in range(7,9)]
-    mdap.MD_Plot(Xname=pc, Xindex=1, Yname=pc, Yindex=0, data_type="pdist", **plot_options).plot()
+    #pc = [f"{variant}/v{i:02d}/0{j}_pcoord.dat" for i in range(1,6) for j in range(7,9)]
+    #mdap.MD_Plot(Xname=pc, Xindex=1, Yname=pc, Yindex=0, data_type="pdist", **plot_options).plot()
 
     #pc = [f"{variant}/rep{i}.dat" for i in range(1,6)]
     #mdap.MD_Plot(Xname=pc, Xindex=0, Yname=pc, Yindex=2, data_type="pdist", **plot_options).plot()
     
-    #pc = [f"{variant}/all_pcoord_{i}.dat" for i in range(1,6)]
+    pc = [f"{variant}/all_pcoord_{i}.dat" for i in range(1,6)]
     #mdap.MD_Plot(Xname=pc, Xindex=1, Yname=pc, Yindex=0, data_type="pdist", last_frame=20000, **plot_options).plot()
-    #mdap.MD_Plot(Xname=pc, Xindex=1, Yname=pc, Yindex=0, data_type="pdist", last_frame=60000, **plot_options).plot()
+    mdap.MD_Plot(Xname=pc, Xindex=1, Yname=pc, Yindex=0, data_type="pdist", last_frame=60000, **plot_options).plot()
     #mdap.MD_Plot(Xname=pc, Xindex=1, Yname=pc, Yindex=0, data_type="pdist", 
     #             first_frame=20000, last_frame=60000, **plot_options).plot()
     #mdap.MD_Plot(Xname=pc, Xindex=1, Yname=pc, Yindex=0, data_type="pdist", 
@@ -90,13 +90,13 @@ if __name__ == "__main__":
     #variants = ["1lst_WT", "1lst_T121A", "1lst_T121K", "1lst_Y14A", "1lst_T121A-Y14A"]
     #variants = ["1lst_WT", "1lst_T121A"]
     #variants = ["1lst_WT"]
-    variants = ["1lst_T121A", "1lst_Y14A", "1lst_WT"]
-    #variants = ["T121A"]
+    #variants = ["1lst_T121A", "1lst_Y14A", "1lst_WT"]
+    variants = ["T121A"]
     for variant in variants:
         wepr_plot(variant, title="")
         #plt.savefig(f"figs/06_pdist_{variant}.pdf")
-        #plt.savefig(f"figs/600ns_pdist_{variant}.pdf")
-        plt.savefig(f"figs/last400ns_pdist_{variant}.pdf")
+        plt.savefig(f"figs/600ns_pdist_{variant}.pdf")
+        #plt.savefig(f"figs/last400ns_pdist_{variant}.pdf")
         #plt.savefig(f"figs/last400ns_1us_pdist_{variant}.pdf")
     ##plt.savefig(f"all_pdist_{variant}.pdf")
 
